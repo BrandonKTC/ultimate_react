e2e312f9e2946ee2b0d7571b13c9f57b36ce8dda
Brandon Kwamou <brandonkwamou@gmail.com>
2024-02-25 09:19:50 +0100

    03-pizza-menu

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -76,7 +76,7 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <>
+        <React.Fragment>
           <p>
             Authentic Italian cuisine. {pizzaData.length} creative dishes to
             choose from our stone oven, all organic, all delicious.
@@ -86,7 +86,7 @@ function Menu() {
               <Pizza key={i} data={pizza} />
             ))}
           </ul>
-        </>
+        </React.Fragment>
       ) : (
         <p>We're still working on our menu. Please come back later 😁</p>
       )}
